ca8557187eaad14f10990557adaaea5aca9e077e
lvapl <clashgoglvap@gmail.com>
2024-09-09 21:45:32 +0300

    Created game

---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    public Color color;
+
+    void Start()
+    {
+        var blocks = GetComponentsInChildren<SpriteRenderer>();
+        foreach (var block in blocks)
+        {
+            block.color = new Color(color.r, color.g, color.b);
+        }
+    }
+}
---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    public float speed = 150;
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
